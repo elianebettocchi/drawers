e0fc91e14c5d859ca79bd42d4e46c36150bd918c
Eliane Bettocchi <elianebettocchi@gmail.com>
2025-04-11 10:47:00 -0400

    Add files via upload

--- cards.js
+++ cards.js
@@ -1,13 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('cards').addEventListener('click', function () {
-        let rolledValue = Math.floor(Math.random() * 48)
+        let rolledValue = Math.floor(Math.random() * 49)
         document.getElementById('cards').src = `./cards/${rolledValue}.png`
 
     })
     addEventListener('click', function () {
         // Reset the rolls values to zeroes
-        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
 
         // Update the scoreboard
         //updateScoreboard()
--- events.js
+++ events.js
@@ -1,13 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('events').addEventListener('click', function () {
-        let rolledValue = Math.floor(Math.random() * 43)
+        let rolledValue = Math.floor(Math.random() * 45)
         document.getElementById('events').src = `./events/${rolledValue}.png`
 
     })
 
     addEventListener('click', function () {
-        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        allRolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     })
 })
@@ -16,4 +16,4 @@ var x = document.getElementById("myAudio");
 
 function playAudio() { 
   x.play(); 
-} 
+}  
