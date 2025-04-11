7768572f15bba70475a3f0d63701f174a819272f
Eliane Bettocchi <elianebettocchi@gmail.com>
2025-04-11 19:09:46 -0400

    Add files via upload

---
+++ status.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('status').addEventListener('click', function () {
+        let drawValue = Math.floor(Math.random() * 6)
+        document.getElementById('status').src = `./status/${drawValue}.png`
+
+    })
+
+    addEventListener('click', function () {
+        allDraws = [0, 0, 0, 0, 0, 0]
+    })
+})
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
